8e8c7f1e052b288edecc405ce82343f3593e1e0b
KVCobbs <kenyacobbs9001@gmail.com>
2020-08-03 18:01:58 -0400

    8320206:01

---
+++ frontend/app.js
@@ -0,0 +1,4 @@
+const insults = document.querySelector('.insults')
+const submit = document.getElementById('submit')
+const editSubmit = document.getElementById('submit-edit')
+const deleteButton = document.getElementByIdById('delete')
