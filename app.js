af248c8a444ba0733d4de01461975f10bd2c2aa2
KVCobbs <kenyacobbs9001@gmail.com>
2020-08-06 12:45:12 -0400

    ccchanges

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const $insultInput = $("#createinput");
 const $insultsSelect = $("#createselect");
 //button with the property create
 const $button = $("#createbutton");
-const $insultEditInput = $("#editinput");
+const $insultEditInput = $("#updateInsult");
 //button with the property edit
-const $editButton = $("#editbutton");
+const $updateButton = $("#updatebutton");
+console.log($updateButton);
 const $ul = $("ul");
+let insults = [] 
 
 //////////////////////////////
 //Define your functions here
@@ -26,6 +28,7 @@ const getInsults = async () => {
   //gets the insults
   const response = await fetch(`${URL}/insults`); //this line connects to line 4
   const data = await response.json();
+  insults = await data; //To bring the data here we brought it from the seed to the app using await data which is the seed.
 
  /*
 The code below helped me put my database on screen and the line below that
@@ -36,21 +39,24 @@ random insult from the seed and put it on the page as a starter for the user.
 
     ////populate DOM with Insult
     // data.forEach((insult) => { 
-      const randomInsultIndex = Math.floor(Math.random()*data.length)
-      //So here is where we made the insults generate randomly using Math.Random and at the end of this line
-      //.attr we are using it to look for the ID of the insult evertime one is generated and makes a new paragrah.
-        const $p = $(`<p>`).text(data[randomInsultIndex].name).attr("id", data[randomInsultIndex]._id)
-        await $p.on('click', console.log("I'm sick of this shit"))
-        $(`.first`).empty().append($p)
-        console.log(data.length);
-      
+      renderInsults()
         
   }
-
+        const renderInsults = () => {
+          console.log("whatever.")
+          const randomInsultIndex = Math.floor(Math.random()*insults.length)
+      //So here is where we made the insults generate randomly using Math.Random and at the end of this line
+      //.attr we are using it to look for the ID of the insult evertime one is generated and makes a new paragrah.
+        const $p = $(`<p>`).text(insults[randomInsultIndex].name).attr("id", insults[randomInsultIndex]._id)
+        $p.on('click',) //Then here we changed the data to insult. so insults = await data meaning it's 
+        $(`.first`).empty().append($p) //Just waiting for the data to come. 
+        console.log(insults.length);
+        }
+      
 //This function makes insults appear on screen
-getInsults();
-//$('#generateInsult').on('click', randomInsultIndex)
+$('#generatebutton').on('click', renderInsults)
 
+      getInsults()
 
               //Create an Insult
         const addtoInsultIndex = async () => {
@@ -94,29 +100,34 @@ getInsults();
       $('#deletebutton').on('click', deleteInsult)
 
 
-      //The idea I was going for with update is that user can update insults. like if you wanted to fix a typo
-    //Update an Insult
-    const updateInsult = async(event) => {
-        const $pupdate = $(`p`).attr("id")
-          
-      //create updated insult Object.
-        const updatedInsult = {
-          "name":$nameEditInput.val()
-        }
-        //make our put request
-       // const response = await fetch(`${URL}/insults/${}`, {
-          //method:"PUT",
-          // headers:{
-            //  "Content-Type":"application/json"
-           // },
-           // body:JSON.stringify(updatedInsults)
-       // })
-   }
+            //The idea I was going for with update is that user can update insults. like if you wanted to fix a typo
+          //Update an Insult
+          const updateInsult = async(event) => {
+            console.log("This is alot of insults jesus christ.");
+              const $pupdate = $(`p`).attr("id")
 
-          //update the DOM
-          $('#updatebutton').on('click', updateInsult)
-          getInsults();
-          
+            //create updated insult Object.
+              const updatedInsult = {
+                "name":$insultEditInput.val()
+              } 
+              console.log(updatedInsult);
+              //make our put request
+              const response = await fetch(`${URL}/insults/${$pupdate}`, {
+                method:"PUT",
+                headers:{
+                  "Content-Type":"application/json"
+                },
+                  body:JSON.stringify(updatedInsult)
+            // })
+        })
+        const data = await response.json()
+        console.log(data);
+        getInsults();
+        }
+                //update the DOM
+                $updateButton.on('click', updateInsult)
+                getInsults();
+                //What I learned with Alex is that update is the hardest and I need to make sure that I double check things 4 times before testing
 
 
     //hamburger menu
@@ -126,4 +137,4 @@ getInsults();
         $('nav').toggle(600)
       });
     });
-
+  
