5b1620a48d44005d903e94d7774b390e2390de85
KVCobbs <kenyacobbs9001@gmail.com>
2020-08-06 22:19:00 -0400

    pushitpushit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //const { link } = require("fs");
 
+//const signIn = require("../Project-2-App-Backend/Models/Signin");
+
 const deployedURL = ""
 const URL = deployedURL ? deployedURL:"http://localhost:3000" //This is the URL being deployed and running
 
@@ -16,6 +18,7 @@ const $updateButton = $("#updatebutton");
 console.log($updateButton);
 const $ul = $("ul");
 let insults = [] 
+let users = [] 
 
 //////////////////////////////
 //Define your functions here
@@ -51,6 +54,7 @@ random insult from the seed and put it on the page as a starter for the user.
         $p.on('click',) //Then here we changed the data to insult. so insults = await data meaning it's 
         $(`.first`).empty().append($p) //Just waiting for the data to come. 
         console.log(insults.length);
+        getUsers()
         }
       
 //This function makes insults appear on screen
@@ -139,22 +143,39 @@ $('#generatebutton').on('click', renderInsults)
       });
     });
   
-/*
-    //////////////////////////////////////////////////////////////////////
-    const $signInInput = $("#create");
-    let users = [] 
 
-    const signIn = async () => {
-      //gets the users
+    //////////////////////////////////////////////////////////////////////
+    const getUsers = async () => {
+      //gets the insults
       const response = await fetch(`${URL}/signIn`); //this line connects to line 4
       const data = await response.json();
-      insults = await data; //To bring the data here we brought it from the seed to the app using await data which is the seed.
-     // data.forEach((insult) => { 
-      const $p = $(`<p>`).text(users[randomInsultIndex].name).attr("id", user[randomInsultIndex]._id)
-      $p.on('click',) //Then here we changed the data to insult. so insults = await data meaning it's 
-      $(`.first`).empty().append($p) //Just waiting for the data to come. 
-      console.log(signIn.length);
+        users = await data; //To bring the data here we brought it from the seed to the app using await data which is the seed.
+    
+     /*
+    The code below helped me put my database on screen and the line below that
+    The line below helped me put all my insults from my seed on screen but what I wanted was a 
+    random insult from my seed so thanks to Jordan I figured out how to use math.random to grab a 
+    random insult from the seed and put it on the page as a starter for the user. 
+    */
+   const renderUsers = () => {
+    console.log("whatever2.")
+    const randomsignInIndex = Math.floor(Math.random()*users.length)
+//So here is where we made the insults generate randomly using Math.Random and at the end of this line
+//.attr we are using it to look for the ID of the insult evertime one is generated and makes a new paragrah.
+  const $p2 = $(`<p>`).text(users[randomsignInIndex].name).attr("id", users[randomsignInIndex]._id)
+  $p2.on('click',) //Then here we changed the data to insult. so insults = await data meaning it's 
+  $(`.first`).append($p2) //Just waiting for the data to come. 
+  console.log(users.length);
+  }
+        ////populate DOM with Insult
+          renderUsers()
+            
       }
+          
+    //This function makes insults appear on screen
+    //$('#generatebutton').on('click', renderUsers)
+    
+        //  getUsers()
 
       /*
               //Create an Insult
